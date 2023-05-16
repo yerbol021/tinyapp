fa5eaae859b56b13e5c6d67c599bc97a038c93f6
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-15 22:03:09 -0700

    added URLs index

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {   
   res.send("Hello!");
 });
 
@@ -19,6 +19,16 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
