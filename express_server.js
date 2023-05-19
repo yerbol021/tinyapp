227088dc5569f0b11b04448b008fa63299ce648c
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-18 22:52:54 -0700

    Only Registered Users Can Shorten URLs

--- express_server.js
+++ express_server.js
@@ -147,16 +147,29 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const userId = req.cookies.user_id;
   const user = users[userId];
-  const templateVars = {
-    urls: urlDatabase,
-    user: user
-  };
-  res.render("urls_new", templateVars);
+  if (!userId || !users[userId]) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new");
+  }
 });
+
+app.post("/urls", (req, res) => {
+  const userId = req.cookies.user_id;
+  if (!userId || !users[userId]) {
+    res.status(401).send("You need to be logged in to create new URLs.");
+  } else {
+    const longURL = req.body.longURL;
+    const shortURL = "/u/" + generateRandomString(); 
+    urlDatabase[shortURL] = longURL;
+    res.redirect("/urls");
+  }
+});
+
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id]; // Assuming urlDatabase is the name of your database
+  const longURL = urlDatabase[id]; 
   const templateVars = { id: id, longURL: longURL };
   res.render("urls_show", templateVars);
 });
